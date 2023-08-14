33641da77873a552b7572add2bfc4934e19b8311
Gabr1elle <gabrielle.santos@mag3s.com>
2023-08-14 14:52:32 -0300

    update of edit part of project

--- src/components/EditarTarefa.vue
+++ src/components/EditarTarefa.vue
@@ -3,9 +3,11 @@
     <div class="modal-body">
       <h4></h4>
       <div v-if="tarefaSelecionada">
-        <input v-model="tarefaEditavel.title"  class="form-control mb-2" />
+        <input v-model="tarefaEditavel.title" class="form-control mb-2" />
         <textarea
-          v-model="tarefaEditavel.text" class="form-control mb-2"
+          v-model="tarefaEditavel.text"
+          class="form-control mb-2"
+          style="max-height: 200px; resize: vertical"
         ></textarea>
       </div>
     </div>
@@ -14,7 +16,12 @@
       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
         Fechar
       </button>
-      <button id="salvar" @click="salvarAlteracoes" data-bs-dismiss="modal" class="btn btn-primary">
+      <button
+        id="salvar"
+        @click="salvarAlteracoes"
+        data-bs-dismiss="modal"
+        class="btn btn-primary"
+      >
         Salvar alterações
       </button>
     </div>
@@ -31,25 +38,41 @@ export default {
       tarefaEditavel: {
         title: this.tarefa.title,
         text: this.tarefa.text,
-      }
-    }
+      },
+    };
   },
   computed: {
     ...mapState(["tarefaSelecionada"]),
   },
+
+  watch: {
+    // Monitora alterações na propriedade "tarefaSelecionada"
+    tarefaSelecionada: {
+      // Configuração para observação profunda
+      deep: true,
+      // Função que será chamada quando houver uma alteração em "tarefaSelecionada"
+      handler(novoValor) {
+        // Atualiza o título da tarefa editável com o novo valor
+        this.tarefaEditavel.title = novoValor.title;
+        // Atualiza o texto da tarefa editável com o novo valor
+        this.tarefaEditavel.text = novoValor.text;
+      },
+    },
+  },
+
   methods: {
     salvarAlteracoes() {
       this.$store.dispatch("atualizarTarefa", {
         completed: this.tarefa.completed,
         id: this.tarefa.id,
         text: this.tarefaEditavel.text,
-        title: this.tarefaEditavel.title
+        title: this.tarefaEditavel.title,
       });
 
       this.tarefaEditavel = {
         title: "",
         text: "",
-      }
+      };
     },
   },
 };
